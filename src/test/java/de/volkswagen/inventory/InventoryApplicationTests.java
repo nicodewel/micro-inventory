7904e5a8c2ad663f080612f01a9dac28b7857c27
Nicolas Weltzel <nwelt13@gmail.com>
2022-06-02 13:48:32 +0200

    init

---
+++ src/test/java/de/volkswagen/inventory/InventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package de.volkswagen.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
